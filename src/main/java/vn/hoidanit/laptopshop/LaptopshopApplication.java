af8bf531c000769f1e258897db63c7baf8359860
Kendy205 <thanhtung002zzz@gmail.com>
2025-11-23 11:03:22 +0700

    set up sercurity

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -3,20 +3,24 @@ package vn.hoidanit.laptopshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+
 // Bỏ kết nối DB
 //@SpringBootApplication(exclude = {
 //        org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
 //        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
 //})
-@SpringBootApplication
+// Bỏ security
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class LaptopshopApplication {
 
-	public static void main(String[] args) {SpringApplication.run(LaptopshopApplication.class, args);
-//        ApplicationContext abc  = SpringApplication.run(LaptopshopApplication.class, args);
-//        for(String beanName : abc.getBeanDefinitionNames())
-//        {
-//            System.out.println(beanName);      ===> BEANS
-//        }
+	public static void main(String[] args) {
+		SpringApplication.run(LaptopshopApplication.class, args);
+		// ApplicationContext abc = SpringApplication.run(LaptopshopApplication.class,
+		// args);
+		// for(String beanName : abc.getBeanDefinitionNames())
+		// {
+		// System.out.println(beanName); ===> BEANS
+		// }
 	}
 
 }
