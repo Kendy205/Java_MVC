ddf4b402a38bdcb22f9e492f1165e909bc18fa46
Kendy205 <thanhtung002zzz@gmail.com>
2025-11-23 11:46:09 +0700

    view engine JSP

--- src/main/java/vn/hoidanit/laptopshop/services/UserService.java
+++ src/main/java/vn/hoidanit/laptopshop/services/UserService.java
@@ -1,7 +1,10 @@
 package vn.hoidanit.laptopshop.services;
 
 import org.springframework.stereotype.Service;
-// Component - Scan (  cùng cấp với application)
+
 @Service
-public class MyService {
+public class UserService {
+    public String handleHomePage() {
+        return "hehe";
+    }
 }
